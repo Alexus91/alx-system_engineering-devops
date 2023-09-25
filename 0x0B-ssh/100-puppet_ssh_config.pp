8d3055ad4d853328ddc7c57d3b2aa1e88ef39820
Alexus91 <mrfadlkamal@gmail.com>
2023-09-24 19:15:55 -0700

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Ensure SSH client configuration using Puppet
+# configuration to connect to a server without typing a password
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+
+# Newline
+
