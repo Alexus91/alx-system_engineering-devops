02567058c226202c5aeb7bd4d9ffd1ed5b175023
Alexus91 <mrfadlkamal@gmail.com>
2023-11-07 16:05:12 -0800

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""queries the Reddit API and prints the titles of 
+the first 10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+
+    Urlapi = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+    lims = 10
+
+    response = requests.get(
+        Urlapi, headers={"user-agent": userAgent}, params={"limit": lims})
+    if not response:
+        print('None')
+        return
+    response = response.json()
+    lis_obj = response['data']['children']
+    for obj in lis_obj:
+        print(obj['data']['title'])
+    return
